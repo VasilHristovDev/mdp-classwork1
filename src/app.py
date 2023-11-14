757edd5929a2614bb9cb58e2be3af9f80d4183d9
Vasil Hristov <vasil.hristov@lab08.com>
2023-11-14 16:45:19 +0200

    unittests

--- src/app.py
+++ src/app.py
@@ -16,9 +16,6 @@ class User(db.Model):
   username = db.Column(db.String(80), unique=True, nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
 
-  def __repr__(self):
-    return f'<User {self.username}>'
-
 
 @app.route('/')
 def hello_world():
