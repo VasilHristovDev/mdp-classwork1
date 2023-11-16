e2ba54489bd74495698b47a5b1786ca8a657174a
Vasil <vortexblast550@gmail.com>
2023-11-16 09:16:36 +0200

    [removed files]

--- src/app.py
+++ src/app.py
@@ -1,20 +1,8 @@
 import socket
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://username:admin123@127.0.0.1:3307/flask'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable modification tracking as it can be resource-intensive
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-
-class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(80), unique=True, nullable=False)
-  email = db.Column(db.String(120), unique=True, nullable=False)
 
 
 @app.route('/')
